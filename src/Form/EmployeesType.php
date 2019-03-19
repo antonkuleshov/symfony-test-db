5f5fdcbb427533bc0eb777e76077a849b4d6ab4d
Anton Kuleshov <antonkuleshov8@gmail.com>
2019-03-19 13:31:59 +0200

    Generate Crud Controllers

---
+++ src/Form/EmployeesType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employees;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('birthDate')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('gender')
+            ->add('hireDate')
+            ->add('deptNo')
+            ->add('deptNoTwo')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Employees::class,
+        ]);
+    }
+}
